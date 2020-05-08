f8254c33a17b399ab70bee1aff79f252a4dc5fdf
robin.keunen <robin@keunen.net>
2020-05-08 17:51:25 +0200

    [ADD] csv_export_base: abstract classes

--- csv_export_invoice/models/csv_export_invoice.py
+++ csv_export_invoice/models/csv_export_invoice.py
@@ -3,14 +3,8 @@
 #   Robin Keunen <robin@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-import base64
-from datetime import datetime
-
-from cStringIO import StringIO
-from openerp import _, api, fields, models
+from openerp import _, models
 from openerp.exceptions import ValidationError
-
-from .csv_writer import CSVUnicodeWriter
 
 HEADERS = (
     "Journal",
@@ -37,21 +31,18 @@ HEADERS = (
 
 class InvoiceCSVExport(models.TransientModel):
     _name = "csv.export.invoice"
+    _inherit = "csv.export.base"
     _description = "Export Invoice CSV"
     _connector_model = "account.invoice"
-
-    def _default_filename(self):
-        now = datetime.now()
-        filename = "FACT_%s.csv" % now.strftime("%Y%m%d_%H%M")
-        return filename.replace("-", "_")
-
-    data = fields.Binary(string="CSV", readonly=True)
-    filename = fields.Char(string="File Name", default=_default_filename)
-    start_date = fields.Date(string="Start Date", required=True)
-    end_date = fields.Date(string="End Date", required=True)
+    _filename_template = "INV_%Y%m%d_%H%M.csv"
 
     def get_domain(self):
-        return [("date", ">=", self.start_date), ("date", "<", self.end_date)]
+        return [
+            ("state", "!=", "draft"),
+            ("state", "!=", "cancel"),
+            ("date", ">=", self.start_date),
+            ("date", "<", self.end_date),
+        ]
 
     def get_headers(self):
         return HEADERS
@@ -108,35 +99,3 @@ class InvoiceCSVExport(models.TransientModel):
         )
         rows = [self.get_row(line) for line in lines]
         return rows
-
-    @api.multi
-    def get_data(self):
-        recordset = self.env[self._connector_model].search(self.get_domain())
-        header = self.get_headers()
-        rows = self.get_rows(recordset)
-        data = [header] + rows
-        return data
-
-    @api.multi
-    def action_manual_export_invoice(self):
-        self.ensure_one()
-        data = self.get_data()
-        # file_data = open("test_file.csv", "w")
-        file_data = StringIO()
-        try:
-            writer = CSVUnicodeWriter(file_data, delimiter="|")
-            writer.writerows(data)
-            file_value = file_data.getvalue()
-            # print(file_value)
-            self.data = base64.encodestring(file_value)
-        finally:
-            file_data.close()
-        return {
-            "type": "ir.actions.act_window",
-            "res_model": "csv.export.invoice",
-            "view_mode": "form",
-            "view_type": "form",
-            "res_id": self.id,
-            "views": [(False, "form")],
-            "target": "new",
-        }
