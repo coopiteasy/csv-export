96dbe74251736ec5863fd7028135f46475740cde
robin.keunen <robin@keunen.net>
2020-05-08 19:30:59 +0200

    [ADD] csv_export_partner

---
+++ csv_export_partner/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Export Partner CSV",
+    "version": "9.0.1.0.0",
+    "depends": ["account", "csv_export_base"],
+    "author": "Coop IT Easy SCRLfs",
+    "summary": "Export your partners as CSV flat files",
+    "website": "http://www.coopiteasy.be",
+    "license": "AGPL-3",
+    "data": ["views/export_csv_partner.xml"],
+    "installable": True,
+}
