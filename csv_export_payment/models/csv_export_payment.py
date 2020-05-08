33e87006d5a3a7cba0b82aa4ebc0aa3eed8d9e36
robin.keunen <robin@keunen.net>
2020-05-08 20:42:39 +0200

    [ADD] csv_export_payment

---
+++ csv_export_payment/models/csv_export_payment.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import models
+
+HEADERS = ("Ref", "JouFin", "Date", "RefCli", "Facture", "Montant")
+
+
+class PartnerCSVExport(models.TransientModel):
+    _name = "csv.export.payment"
+    _inherit = "csv.export.base"
+    _description = "Export Partner CSV"
+    _connector_model = "account.payment"
+    _filename_template = "CASH_%Y%m%d_%H%M.csv"
+
+    def get_domain(self):
+        return [
+            ("journal_id.code", "=", "CSH1"),
+            ("state", "!=", "draft"),
+            ("payment_date", ">=", self.start_date),
+            ("payment_date", "<", self.end_date),
+        ]
+
+    def get_headers(self):
+        return HEADERS
+
+    def get_row(self, record):
+        payment = record
+
+        invoices = ",".join(payment.invoice_ids.mapped("number"))
+
+        rows = (
+            payment.name,
+            payment.journal_id.code,
+            payment.payment_date,
+            payment.partner_id.name,
+            invoices,
+            payment.amount,
+        )
+        return rows
