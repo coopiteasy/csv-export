03586071f2e78843c5a09f0547c6e5fd7cffa503
robin.keunen <robin@keunen.net>
2020-05-19 17:27:30 +0200

    [ADD] sftp_backend

---
+++ sftp_backend/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "SFTP Backend",
+    "version": "9.0.1.0.0",
+    "depends": [],
+    "author": "Coop IT Easy SCRLfs",
+    "summary": "SFTP backend utils.",
+    "website": "http://www.coopiteasy.be",
+    "license": "AGPL-3",
+    "data": ["security/ir.model.access.csv", "views/menu.xml"],
+    "external_dependencies": {"python": ["paramiko"]},
+    "installable": True,
+}
---
+++ sftp_backend/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Utility module to upload files on a backend server.
+
+Configure the STFP server credentials in the technical conofiguration menu.
