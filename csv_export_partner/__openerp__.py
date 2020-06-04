aeb81f09c65fa47a95d0ce5ae58d77ad1f274ed8
robin.keunen <robin@keunen.net>
2020-06-04 15:00:12 +0200

    [FIX] ceb: add account dependency

--- csv_export_partner/__openerp__.py
+++ csv_export_partner/__openerp__.py
@@ -5,7 +5,7 @@
 {
     "name": "Export Partner CSV",
     "version": "9.0.1.0.0",
-    "depends": ["account", "csv_export_base"],
+    "depends": ["csv_export_base"],
     "author": "Coop IT Easy SCRLfs",
     "summary": "Export your partners as CSV flat files",
     "website": "https://www.coopiteasy.be",
