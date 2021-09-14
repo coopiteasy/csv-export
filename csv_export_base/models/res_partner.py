dbbea67f861c6bc1ba8207f2fe9a9ff5b9095c10
robin.keunen <robin@keunen.net>
2021-09-14 17:41:19 +0200

    [IMP] csv_export_base: getter for _export_reference

--- csv_export_base/models/res_partner.py
+++ csv_export_base/models/res_partner.py
@@ -2,7 +2,7 @@
 #   Robin Keunen <robin@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -11,18 +11,26 @@ class ResPartner(models.Model):
     _sql_constraints = [
         (
             "partner_export_reference_unique",
-            "UNIQUE (export_reference)",
+            "UNIQUE (_export_reference)",
             "The export reference must be unique!",
         ),
     ]
 
-    def _default_export_reference(self):
+    def _next_export_reference(self):
         return self.env["ir.sequence"].next_by_code("res.partner.export.reference")
 
-    export_reference = fields.Char(
+    @api.multi
+    def get_export_reference(self):
+        self.ensure_one()
+        if self._export_reference:
+            return self._export_reference
+        else:
+            export_reference = self._next_export_reference()
+            self._export_reference = export_reference
+            return export_reference
+
+    _export_reference = fields.Char(
         string="Export Reference",
-        default=_default_export_reference,
-        required=True,
         read_only=True,
         copy=False,
     )
--- csv_export_partner/hooks.py
+++
@@ -1,6 +0,0 @@
-def set_default_export_reference(cr, registry):
-    partner_obj = registry["res.partner"]
-    partner_ids = partner_obj.search(cr, 1, [("export_reference", "=", False)])
-    partners = partner_obj.browse(cr, 1, partner_ids)
-    for partner in partners:
-        partner.export_reference = partner._default_export_reference()
